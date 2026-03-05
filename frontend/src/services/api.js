72519cef49522a9e4288aaada7a447f2aeda9ec2
Michael Oliveira <michael.veneza@gmail.com>
2026-03-05 17:59:12 -0400

    fit :porta 3001 ára 5000

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@
 
 // frontend/src/services/api.js
 export const API_URL = 
-  import.meta.env.VITE_API_URL || "http://localhost:3001/api";
+  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
 
 
 function getToken() {
